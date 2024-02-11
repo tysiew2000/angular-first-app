30ae235526e0854d0bd3e2e8b0d77096b27bdbdf
tysiew2000 <tysiew2000@gmail.com>
2024-02-11 00:28:14 +0000

    added routing

--- angular-app/src/app/home/home.component.ts
+++ angular-app/src/app/home/home.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject  } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housing-location';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HousingLocationComponent],
+  imports: [HousingLocationComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
 
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
 }
--- angular-app/src/app/housing-location/housing-location.component.ts
+++ angular-app/src/app/housing-location/housing-location.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housing-location';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
